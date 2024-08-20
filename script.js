5e5e0607b5b9d10de19d275aaf1bedaca32d9479
Snehalatha34 <144024222+Snehalatha34@users.noreply.github.com>
2024-08-20 16:32:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const container = document.getElementById('container');
+const text = document.getElementById('text');
+const button = document.getElementById('toggleButton');
+
+button.addEventListener('click', () => {
+    if (container.style.backgroundColor === 'skyblue' || container.style.backgroundColor === 'aqua') {
+        container.style.backgroundColor = 'lightgreen';
+        text.textContent = 'This is the initial text that has to be changed.';
+        button.textContent = 'Submit';
+        button.style.backgroundColor = 'blue';
+    } else {
+        container.style.backgroundColor = 'skyblue'; 
+        text.textContent = 'New text that has changed from initial text.';
+        button.textContent = 'Reset';
+        button.style.backgroundColor = 'green';
+    }
+});
